c3160d4caa1296114f7b01c75f438d4ef1a3f1d9
SimHanssens <sim.hanssens@student.ehb.be>
2021-08-16 16:46:19 +0200

    HTML - CSS - Error handlers

---
+++ js/Articles.js
@@ -0,0 +1,50 @@
+class Artikel {
+    constructor(ID, titel, content, imageURL, likes, datum){
+        this._ID = ID;
+        this._titel = titel;
+        this._content = content;
+        this._image = imageURL;
+        this._likes = likes;
+        this._datum = datum;
+    }
+    set id (ID){
+        this._ID = ID;
+    }
+    get id (){
+        return this._ID;
+    }
+    set titel (titel){
+        this._titel = titel;
+    }
+    get titel (){
+        return this._titel;
+    }    
+    set content (content){
+        this._content = content;
+    }
+    get content (){
+        return this.content;
+    }
+    set image (imageURL){
+        this._image = imageURL;
+    }
+    get image (){
+        return this._image;
+    }
+    set likes (likes){
+        this._likes = likes;
+    }
+    get likes (){
+        return this._likes;
+    }
+    set datum (datum){
+        this._datum = datum;
+    }
+    get datum (){
+        return this.datum;
+    }
+    like (){
+        
+    }
+
+}
--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const niewsArtikels = {
+    initfields() {
+        document.getElementById("form").addEventListener("submit", e=> {
+            e.preventDefault();
+            let inputValue = document.getElementById("searchKeyword").nodeValue;
+            this.dataApi(inputValue);
+        });
+    },
+    dataApi(keyword){
+
+        console.log("start loading sequence");
+        document.getElementById("content").innerHTML = "loading";
+        fetch('https://thecrew.cc/news/read.php')
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
+        .then(data => {
+            console.log(response);
+           const likes = data.docs;
+           if(data.numFound == 0){
+            
+           }
+        })
+    }
+
+}
